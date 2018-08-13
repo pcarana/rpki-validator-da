06b91546c98bc62910f4d43ad1a9eaa71eefb23f
Paco Moreno <pc.moreno2099@gmail.com>
2018-08-13 12:53:58 -0500

    Update due to RFC 8416 release ('publicKey' is now 'routerPublicKey')

--- src/main/java/mx/nic/lab/rpki/db/pojo/SlurmBgpsec.java
+++ src/main/java/mx/nic/lab/rpki/db/pojo/SlurmBgpsec.java
@@ -13,7 +13,7 @@ public class SlurmBgpsec extends ApiObject {
 	public static final String ID = "id";
 	public static final String ASN = "asn";
 	public static final String SKI = "ski";
-	public static final String PUBLIC_KEY = "publicKey";
+	public static final String ROUTER_PUBLIC_KEY = "routerPublicKey";
 	public static final String TYPE = "type";
 	public static final String COMMENT = "comment";
 
@@ -39,9 +39,9 @@ public class SlurmBgpsec extends ApiObject {
 	private String ski;
 
 	/**
-	 * Text representation of the Public Key
+	 * Text representation of the Router's Public Key
 	 */
-	private String publicKey;
+	private String routerPublicKey;
 
 	/**
 	 * BGPsec type (filter or assertion)
@@ -68,7 +68,7 @@ public class SlurmBgpsec extends ApiObject {
 		sb.append(", ");
 		sb.append(SKI).append("=").append(ski != null ? ski : "null");
 		sb.append(", ");
-		sb.append(PUBLIC_KEY).append("=").append(publicKey != null ? publicKey : "null");
+		sb.append(ROUTER_PUBLIC_KEY).append("=").append(routerPublicKey != null ? routerPublicKey : "null");
 		sb.append(", ");
 		sb.append(TYPE).append("=").append(type != null ? type : "null");
 		sb.append(", ");
@@ -84,7 +84,7 @@ public class SlurmBgpsec extends ApiObject {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((asn == null) ? 0 : asn.hashCode());
 		result = prime * result + ((ski == null) ? 0 : ski.hashCode());
-		result = prime * result + ((publicKey == null) ? 0 : publicKey.hashCode());
+		result = prime * result + ((routerPublicKey == null) ? 0 : routerPublicKey.hashCode());
 		result = prime * result + ((type == null) ? 0 : type.hashCode());
 		result = prime * result + ((comment == null) ? 0 : comment.hashCode());
 		return result;
@@ -114,10 +114,10 @@ public class SlurmBgpsec extends ApiObject {
 				return false;
 		} else if (!ski.equals(other.ski))
 			return false;
-		if (publicKey == null) {
-			if (other.publicKey != null)
+		if (routerPublicKey == null) {
+			if (other.routerPublicKey != null)
 				return false;
-		} else if (!publicKey.equals(other.publicKey))
+		} else if (!routerPublicKey.equals(other.routerPublicKey))
 			return false;
 		if (type == null) {
 			if (other.type != null)
@@ -156,12 +156,12 @@ public class SlurmBgpsec extends ApiObject {
 		this.ski = ski;
 	}
 
-	public String getPublicKey() {
-		return publicKey;
+	public String getRouterPublicKey() {
+		return routerPublicKey;
 	}
 
-	public void setPublicKey(String publicKey) {
-		this.publicKey = publicKey;
+	public void setRouterPublicKey(String routerPublicKey) {
+		this.routerPublicKey = routerPublicKey;
 	}
 
 	public Integer getType() {

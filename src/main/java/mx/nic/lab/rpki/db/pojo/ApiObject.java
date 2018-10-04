69781cc196eb9a9f8b5e15dd32e772784b9b7679
Paco Moreno <pc.moreno2099@gmail.com>
2018-10-04 08:31:46 -0500

    Add ASN min and max value permitted so that others can use it

--- src/main/java/mx/nic/lab/rpki/db/pojo/ApiObject.java
+++ src/main/java/mx/nic/lab/rpki/db/pojo/ApiObject.java
@@ -6,4 +6,16 @@ package mx.nic.lab.rpki.db.pojo;
  */
 public abstract class ApiObject {
 
+	/**
+	 * Minimum value permitted by an ASN, declared here since multiple objects can
+	 * use it
+	 */
+	public static final long ASN_MIN_VALUE = 0L;
+
+	/**
+	 * Maximum value permitted by an ASN, declared here since multiple objects can
+	 * use it
+	 */
+	public static final long ASN_MAX_VALUE = 4294967295L;
+
 }

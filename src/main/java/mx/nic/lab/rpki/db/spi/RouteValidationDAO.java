39a5c6114ee0ba56bd6131cd57c99a9368e4b510
Paco Moreno <pc.moreno2099@gmail.com>
2018-12-05 16:33:28 -0600

    Add 'fullCheck' to indicate if the implementation must do a heavy search

--- src/main/java/mx/nic/lab/rpki/db/spi/RouteValidationDAO.java
+++ src/main/java/mx/nic/lab/rpki/db/spi/RouteValidationDAO.java
@@ -10,11 +10,21 @@ import mx.nic.lab.rpki.db.pojo.RouteValidation;
 public interface RouteValidationDAO extends DAO {
 
 	/**
-	 * Validate a prefix against the validated ROAs
+	 * Validate a prefix against the SLURM and validated ROAs
 	 * 
+	 * @param asn
+	 *            ASN
+	 * @param prefix
+	 *            Prefix
+	 * @param prefixLength
+	 *            Prefix length
+	 * @param fullCheck
+	 *            Indicate if a full check will be done, if <code>false</code> then
+	 *            the search must be made looking for the exact match
 	 * @return The result of the validation as a {@link RouteValidation}
 	 * @throws ApiDataAccessException
 	 */
-	public RouteValidation validate(Long asn, byte[] prefix, Integer prefixLength) throws ApiDataAccessException;
+	public RouteValidation validate(Long asn, byte[] prefix, Integer prefixLength, boolean fullCheck)
+			throws ApiDataAccessException;
 
 }
